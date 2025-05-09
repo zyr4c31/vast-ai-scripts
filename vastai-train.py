138a46cfb3d156d49d59e68550bf4947da100c7f
zyr4c31 <zyr4c31@gmail.com>
2025-05-09 16:49:42 +0800

    update

--- vastai-train.py
+++ vastai-train.py
@@ -4,15 +4,15 @@ import os
 
 rf = Roboflow(api_key=os.getenv("ROBOFLOW_API_KEY"))
 
-project = rf.workspace("floorplan-y38xx").project("rooms-bb")
-version = project.version(10)
+project = rf.workspace("floorplan-y38xx").project("floorplans-seg")
+version = project.version(7)
 dataset = version.download("yolov11")
 
-room_model_name = "rooms-bb"
-save_file_name= f"/workspace/{room_model_name}_v1.pt"
-version = 10
+room_model_name = "floorplan"
+save_file_name= f"/workspace/{room_model_name}_v10.pt"
+dataset_version = 7
 
 model = YOLO("yolo11x.pt")
-results = model.train(data=f"/workspace/{room_model_name}-{version}/data.yaml")
+results = model.train(data=f"/workspace/{room_model_name}-{dataset_version}/data.yaml")
 
 model.save(save_file_name)
